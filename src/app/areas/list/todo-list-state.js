a3468ca8959c23f2d4afee77cba02dd622b490a1
John Weisz <jana_tb@live.com>
2018-06-24 19:06:08 +0200

    removed hardcoded intial items

--- src/app/areas/list/todo-list-state.js
+++ src/app/areas/list/todo-list-state.js
@@ -15,30 +15,3 @@ export const todoListInitialState = {
     /** @type {Number} */
     idCounter: 0
 };
-
-todoListInitialState.items.set(0, {
-    id: 0,
-    title: "Sketch todo-app",
-    description: "Create the necessary diagrams, basic design concepts, and tech requirements for the todo app.",
-    deadline: Date.now() + 60000,
-    isDone: false
-});
-
-todoListInitialState.items.set(1, {
-    id: 1,
-    title: "Design todo-app UI & UX",
-    description: "Sketch and detail how the todo-app will look, and how it will function from the large picture.",
-    deadline: Date.now() + 600000,
-    isDone: false,
-    isHighPriority: true
-});
-
-todoListInitialState.items.set(2, {
-    id: 2,
-    title: "Finish todo-app",
-    description: "Implement ideas and finish the todo-app.",
-    deadline: Date.now() + 1800000,
-    isDone: false
-});
-
-todoListInitialState.idCounter = todoListInitialState.items.size;
